2ecec36c5408225e18fd6ef3ee3fe115356677ae
RazvanFilote <razvan.filote@stud.ubbcluj.ro>
2026-03-31 15:35:00 +0300

    Add shell navigation, active nav state, remove duplicate navbars

--- matchmaking/ViewModels/ShellViewModel.cs
+++ matchmaking/ViewModels/ShellViewModel.cs
@@ -1,17 +1,38 @@
+using System;
 using System.Windows.Input;
 
 namespace matchmaking.ViewModels;
 
 public class ShellViewModel : ObservableObject
 {
-    public ICommand RecommendationsCommand { get; }
-    public ICommand MyStatusCommand { get; }
-    public ICommand ChatCommand { get; }
+    private string _activePage = "MyStatus";
 
-    public ShellViewModel()
+    public ICommand RecommendationsCommand { get; }
+    public ICommand MyStatusCommand        { get; }
+    public ICommand ChatCommand            { get; }
+
+    public string ActivePage
     {
-        RecommendationsCommand = new RelayCommand(() => { });
-        MyStatusCommand = new RelayCommand(() => { });
-        ChatCommand = new RelayCommand(() => { });
+        get => _activePage;
+        set
+        {
+            if (SetProperty(ref _activePage, value))
+            {
+                OnPropertyChanged(nameof(IsRecommendationsActive));
+                OnPropertyChanged(nameof(IsMyStatusActive));
+                OnPropertyChanged(nameof(IsChatActive));
+            }
+        }
+    }
+
+    public bool IsRecommendationsActive => ActivePage == "Recommendations";
+    public bool IsMyStatusActive        => ActivePage == "MyStatus";
+    public bool IsChatActive            => ActivePage == "Chat";
+
+    public ShellViewModel(Action onRecommendations, Action onMyStatus, Action onChat)
+    {
+        RecommendationsCommand = new RelayCommand(onRecommendations);
+        MyStatusCommand        = new RelayCommand(onMyStatus);
+        ChatCommand            = new RelayCommand(onChat);
     }
 }
